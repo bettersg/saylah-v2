b5fa1fe5b46c7695eeea6f18c3ae3442186de82e
Prakash <prakashcse100@gmail.com>
2024-04-09 20:53:14 +0800

    login and signup with aws cognito.

--- saylah-v2-be/index.js
+++ saylah-v2-be/index.js
@@ -1,6 +1,2 @@
 const { registerUser } = require('./controller/logincontroller');
 
-registerUser('example1@example.com', 'myPassword@123', 'ExampleNickname', '+91727273727', 'ExampleAddress', 'ExampleName')
-  .then(console.log)
-  .catch(console.error);
-
